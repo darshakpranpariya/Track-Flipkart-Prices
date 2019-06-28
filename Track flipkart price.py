dda76ab60c2cf3262010a9bea2d2df8257d6a2ec
Darshak Ranpariya <34344691+darshakpranpariya@users.noreply.github.com>
2019-06-28 11:55:05 +0530

    Update Track flipkart price.py

--- Track flipkart price.py
+++ Track flipkart price.py
@@ -25,7 +25,7 @@ def send_mail(title):
 		server.starttls() #It's a command that start our connection with gamil server connection.
 		server.ehlo()
 
-		server.login("darshak.patidar7@gmail.com","darshak@78")
+		server.login("your email id","password")
 
 		subject = "Hey,Price Fall Down of :"+title
 		body = 'check flipkart link: https://www.flipkart.com/optimum-nutrition-gold-standard-100-whey-protein/p/itmeavtn8uhzgxez?pid=PSLEAVTNUGYWHRCZ&lid=LSTPSLEAVTNUGYWHRCZUMIX9W&marketplace=FLIPKART&srno=s_1_2&otracker=search&otracker1=search&fm=SEARCH&iid=881438ca-26e9-42f7-a512-52d6733ef68e.PSLEAVTNUGYWHRCZ.SEARCH&ppt=sp&ppn=sp&ssid=gulco9wm2o0000001561697716715&qH=e385ce1fca93bc49'
@@ -33,8 +33,8 @@ def send_mail(title):
 
 		msg=EmailMessage()
 		msg['Subject'] = subject
-		msg['From'] = "darshak.patidar7@gmail.com"
-		msg['To'] = 'darshak.ranpariya@gmail.com'
+		msg['From'] = "set from email id"
+		msg['To'] = 'set to email id'
 		msg.set_content(body)
 		server.send_message(msg)
 		print("Email sended")
